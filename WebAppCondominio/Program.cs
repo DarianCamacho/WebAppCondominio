c1c7c8594d26c92325eafc6d876a22a113c6c9bd
Darian Camacho <delgadodarian2003@gmail.com>
2023-10-17 19:29:46 -0600

    Version0.1

--- WebAppCondominio/Program.cs
+++ WebAppCondominio/Program.cs
@@ -12,7 +12,7 @@ Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
 
 builder.Services.AddSession(options =>
 {
-	options.IdleTimeout = TimeSpan.FromSeconds(10);
+	options.IdleTimeout = TimeSpan.FromSeconds(3000);
 	options.Cookie.HttpOnly = true;
 	options.Cookie.IsEssential = true;
 });
